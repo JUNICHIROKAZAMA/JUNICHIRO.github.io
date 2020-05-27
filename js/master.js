80b15dd8519dbdbccabdbb2a9e740ab7b9822f9f
JUNICHIROKAZAMA <65481932+JUNICHIROKAZAMA@users.noreply.github.com>
2020-05-27 21:04:12 +0900

    Add files via upload

---
+++ js/master.js
@@ -0,0 +1,68 @@
+// 各データ
+
+const place = [
+  {
+    "name": "Peru",
+    "Etymology": "he English name Switzerland is a compound containing Switzer, an obsolete term for the Swiss, which was in use during the 16th to 19th centuries.",
+		"History": "Switzerland has existed as a state in its present form since the adoption of the Swiss Federal Constitution in 1848.",
+		"Geography": "Extending across the north and south side of the Alps in west-central Europe, Switzerland encompasses a great diversity of landscapes and climates on a limited area of 41,285 square kilometres (15,940 sq mi).",
+		"Wikipedia":"https://en.wikipedia.org/wiki/Peru",
+		"travelsite":"https://www.tour.ne.jp/w_tour/list/10/38/",
+  },
+  {
+    "name": "Malaysia",
+    "Etymology": "The name 'Malaysia' is a combination of the word 'Malay' and the Latin-Greek suffix '-sia'",
+		"History": "Evidence of modern human habitation in Malaysia dates back 40,000 years.",
+		"Geography": "Malaysia is the 66th largest country by total land area, with a land area of 329,613 km2 (127,264 sq mi).",
+		"Wikipedia":"https://en.wikipedia.org/wiki/Malaysia",
+		"travelsite":"https://www.hankyu-travel.com/air/malaysia/",
+  },
+  {
+    "name": "USA",
+    "Etymology": "The first known use of the name 'America' dates back to 1507, when it appeared on a world map created by the German cartographer Martin Waldseemüller.",
+		"History": "Main articles: History of the United States, Timeline of United States history, American business history, Economic history of the United States, and Labor history of the United States",
+		"Geography": "The land area of the entire United States is approximately 3,800,000 square miles (9,841,955 km2),",
+		"Wikipedia":"https://en.wikipedia.org/wiki/United_States",
+		"travelsite":"https://www.hankyu-travel.com/cruise/north-america/",
+  },
+  {
+    "name": "Iceland",
+    "Etymology": "The Sagas of Icelanders say that a Norwegian named Naddodd (or Naddador) was the first Norseman to reach Iceland",
+		"History": "According to both Landnámabók and Íslendingabók, monks known as the Papar lived in Iceland before Scandinavian settlers arrived",
+		"Geography": "Iceland is at the juncture of the North Atlantic and Arctic Oceans. The main island is entirely south of the Arctic Circle,",
+		"Wikipedia":"https://en.wikipedia.org/wiki/Iceland",
+		"travelsite":"https://www.hankyu-travel.com/iceland/",
+  },
+  {
+    "name": "indonesia",
+    "Etymology": "The name dates to the 18th century, far predating the formation of independent Indonesia",
+		"History": "Fossilised remains of Homo erectus, popularly known as the 'Java Man', ",
+		"Geography": "Tectonically, Indonesia is highly unstable, making it a site of numerous volcanoes and frequent earthquakes",
+		"Wikipedia":"https://en.wikipedia.org/wiki/Indonesia",
+		"travelsite":"https://www.hankyu-travel.com/air/indonesia/",
+  },
+  {
+    "name": "Japan",
+    "Etymology": "The Japanese word for Japan is 日本, which is pronounced Nihon or Nippon and literally means 'the origin of the sun'",
+		"History": "A Paleolithic culture around 30,000 BC constitutes the first known habitation of the Japanese archipelago",
+		"Geography": "Japan has a total of 6,852 islands extending along the Pacific coast. It is over 3,000 km (1,900 mi)",
+		"Wikipedia":"https://en.wikipedia.org/wiki/Japan",
+		"travelsite":"https://www.hankyu-travel.com/japan-heritage/36/",
+  },
+  {
+    "name": "United Kingdom",
+    "Etymology": "The 1707 Acts of Union declared that the kingdoms of England and Scotland were 'United into One Kingdom by the Name of Great Britain'.",
+		"History": "Settlement by anatomically modern humans of what was to become the United Kingdom occurred in waves beginning by about 30,000 years ago.",
+		"Geography": "The total area of the United Kingdom is approximately 244,820 square kilometres (94,530 sq mi)",
+		"Wikipedia":"https://en.wikipedia.org/wiki/United_Kingdom",
+		"travelsite":"https://www.hankyu-travel.com/air/england/",
+  },
+  {
+    "name": "Italia",
+    "Etymology": "Hypotheses for the etymology of the name 'Italia' are numerous.",
+		"History": "Thousands of Paleolithic-era artifacts have been recovered from Monte Poggiolo and dated to around 850,000 years before the present",
+		"Geography": "The country's total area is 301,230 square kilometres (116,306 sq mi), of which 294,020 km2 (113,522 sq mi)",
+		"Wikipedia":"https://en.wikipedia.org/wiki/Italy",
+		"travelsite":"https://www.hankyu-travel.com/italy/",
+  },
+]
